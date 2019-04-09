1d68835e0be7024aa06799ef358fb65685247ca1
汪半斤 <1063237332@qq.com>
2019-04-09 18:31:26 +0800

    v2.0

--- mblog/mainsite/admin.py
+++ mblog/mainsite/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post,Product
 # Register your models here.
 
 #admin.site.register(Post)
@@ -9,3 +9,5 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ['title','slug','pub_date']
 
 admin.site.register(Post,PostAdmin)
+
+admin.site.register(Product)
--- mblog/mainsite/models.py
+++ mblog/mainsite/models.py
@@ -21,3 +21,19 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Product(models.Model):
+    SIZE = (
+        ('S','Smaill'),
+        ('M','Medium'),
+        ('L','Large'),
+    )
+
+    sku = models.CharField(max_length=200)
+    name = models.CharField(max_length=10)
+    price = models.PositiveIntegerField()
+    size = models.CharField(max_length=1,choices=SIZE)
+
+    def __str__(self):
+        return self.name
